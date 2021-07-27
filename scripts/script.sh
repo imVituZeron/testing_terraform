5b28159a0399c59e843aa5ee65a3e0ff1efc0483
vitor <vitor@this.this>
2021-07-27 12:28:06 -0300

    creating config files and modifying the main script to insert config files at instances

--- scripts/script.sh
+++ scripts/script.sh
@@ -18,28 +18,42 @@
 PUBLIC_DNS=$(cat terraform.tfstate | grep "public_dns" | cut -d'"' -f4)
 INSTANCE_NAME="ubuntu"
 PATH_SCRIPT="./scripts/install_ansible.sh"
-PATH_SCRIPT_ANSIBLE = "./scripts/playbook.yml"
+PATH_SCRIPT_ANSIBLE="./scripts/playbook.yml"
 COUNT=0
 # ------------------------------------------------------------------ #
 # ---------------------------- Functions --------------------------- #
 insert_files_in_instance(){
-   sudo chmod 777 $PATH_SCRIPT
+   echo "---------- slave -----------------"
    scp $PATH_SCRIPT $INSTANCE_NAME@$VALUE:/home/$INSTANCE_NAME
+   scp ./text_files/public_dns_master.txt $INSTANCE_NAME@$VALUE:/home/$INSTANCE_NAME
+   scp ./scripts/config_slaves.sh $INSTANCE_NAME@$VALUE:/home/$INSTANCE_NAME
+   echo "---------------------------"
 }
 
 insert_files_in_instance_master(){
-   sudo chmod 777 $PATH_SCRIPT
-   sudo chmod 777 $PATH_SCRIPT_ANSIBLE
+   echo "------------ master ---------------"
    scp $PATH_SCRIPT $INSTANCE_NAME@$VALUE:/home/$INSTANCE_NAME
    scp $PATH_SCRIPT_ANSIBLE $INSTANCE_NAME@$VALUE:/home/$INSTANCE_NAME
+   scp ./text_files/public_dns_slave.txt $INSTANCE_NAME@$VALUE:/home/$INSTANCE_NAME
+   scp ./scripts/config_master.sh $INSTANCE_NAME@$VALUE:/home/$INSTANCE_NAME
+   echo "---------------------------"
 }
 # ------------------------------------------------------------------ #
 # --------------------------- Main --------------------------------- #
+sudo chmod 777 $PATH_SCRIPT
+sudo chmod 777 $PATH_SCRIPT_ANSIBLE
+sudo chmod 777 ./text_files/public_dns_slave.txt
+sudo chmod 777 ./text_files/public_dns_master.txt
+sudo chmod 777 ./scripts/config_master.sh
+sudo chmod 777 ./scripts/config_slaves.sh
+
 for VALUE in $(echo $PUBLIC_DNS); do 
    echo "Instance = $VALUE"
    if [ $COUNT -eq 2 ]; then
+      echo $VALUE >> ./text_files/public_dns_master.txt
       insert_files_in_instance_master
    else
+      echo $VALUE >> ./text_files/public_dns_slave.txt
       insert_files_in_instance
    fi
    COUNT=$[$COUNT + 1]
